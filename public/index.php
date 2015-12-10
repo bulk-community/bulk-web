dc8780dc4ed6e370af0a1bef8819c353b0e5d516
Nemanja Nedeljković <nemanjan00@gmail.com>
2015-12-10 19:57:46 +0100

    init

--- public/index.php
+++ public/index.php
@@ -6,11 +6,6 @@ require_once __DIR__.'/../vendor/autoload.php';
 $loader = new Twig_Loader_Filesystem('../templates/');
 $twig = new Twig_Environment($loader);
 
-// JSON REST API
-
-$userId = "1665327528";
-$userToken = "CAAF1hf9heNYBAKZCaLPzSPlmYaUZATANatT9uLZB5dlj4t7f8WbpRDA2oY6aEfZABLWTg5sE36T2PSyMMV9wHWOKj7WNnbFO5s2WX41rt9o8tJaHUUHh5NUY3Vz5O8M7ZBXdFFh8Y5t15gRXKCMie1IBIZAxJLPmUN8M1gHfascZAH7oZBDRjrApVjxU5Tdbh6EZD";
-
 //Router
 
 $klein = new \Klein\Klein();
@@ -26,98 +21,6 @@ $klein->respond('GET', '/app/[*]', function () {
 	global $twig;
 
 	return $twig->render('index.html', array('name' => 'nemanjan00'));
-});
-
-$klein->with('/api', function () use ($klein) {
-	$klein->respond('GET', '/quest-opener', function ($request, $response) {
-		header('Content-Type: application/json');
-		header("Access-Control-Allow-Origin: *");
-
-		$id = "847873595325488";
-
-		global $userId, $userToken;
-
-		$api = new \FacebookMining\FacebookAPI($userId, $userToken);
-
-		$query = Array(
-			"location" => "$id/comments",
-			"parms" => Array(
-				"limit" => 50
-			)
-		);
-
-		$data = $api->request($query)->data;
-
-		$data = $data["data"];
-
-		$openers = Array();
-
-		foreach($data as $comment){
-	        $openers[] = $comment["message"];
-		}
-
-		echo json_encode($openers);
-    });
-	
-	$klein->respond('GET', '/cold-read', function ($request, $response) {
-		header('Content-Type: application/json');
-		header("Access-Control-Allow-Origin: *");
-
-		$id = "847181492061365";
-
-		global $userId, $userToken;
-
-		$api = new \FacebookMining\FacebookAPI($userId, $userToken);
-
-		$query = Array(
-			"location" => "$id/comments",
-			"parms" => Array(
-				"limit" => 50
-			)
-		);
-
-		$data = $api->request($query)->data;
-
-		$data = $data["data"];
-
-		$openers = Array();
-
-		foreach($data as $comment){
-	        $openers[] = $comment["message"];
-		}
-
-		echo json_encode($openers);
-    });
-
-	$klein->respond('GET', '/opener', function ($request, $response) {
-		header('Content-Type: application/json');
-		header("Access-Control-Allow-Origin: *");
-
-		$id = "846651678781013";
-
-		global $userId, $userToken;
-
-		$api = new \FacebookMining\FacebookAPI($userId, $userToken);
-
-		$query = Array(
-			"location" => "$id/comments",
-			"parms" => Array(
-				"limit" => 50
-			)
-		);
-
-		$data = $api->request($query)->data;
-
-		$data = $data["data"];
-
-		$openers = Array();
-
-		foreach($data as $comment){
-	        $openers[] = $comment["message"];
-		}
-
-		echo json_encode($openers);
-    });
 });
 
 // Handle errors
